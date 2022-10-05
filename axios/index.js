66730c0af27afc28617eea859d2d169bf086f928
Carlos Sierra <carlossierra850@gmail.com>
2022-10-05 00:30:11 -0400

    filter the products by categories

--- axios/index.js
+++ axios/index.js
@@ -1,3 +1,7 @@
 import axios from 'axios'
-
-export const axiosClient = axios.create({ baseURL: 'http://localhost:4000/' })
+const backendApi = new URL(
+  process.env.NODE_ENV === 'production'
+    ? process.env.NEXT_PUBLIC_MYAPP_BACKEND
+    : process.env.NEXT_PUBLIC_MYAPP_BACKEND_LOCAL
+)
+export const axiosClient = axios.create({ baseURL: backendApi.origin })
